68e7a98e523611825975a4adc43ab25c9c3eeed2
Evolveye <stolarski.paw@gmail.com>
2020-10-21 14:32:00 +0200

    Function colors changer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-function submit() {
+document.querySelectorAll( `.form-button.is-submit` ).forEach( button => {
+  button.addEventListener( `click`, e => {
+    e.preventDefault()
 
-}
+    let form = button.parentElement
+
+    while (form.tagName != `FORM`) form = form.parentElement
+
+    const { red, green, blue, alpha } = form.elements
+
+    const alphaSaturation = red.checked || green.checked || blue.checked ? `8` : `0`
+
+    form.querySelectorAll( `.form-colors-wrapper > div` ).forEach( wrapper => {
+      wrapper.style.backgroundColor = `#`
+        + (red.checked   ? `f` : `0`)
+        + (green.checked ? `f` : `0`)
+        + (blue.checked  ? `f` : `0`)
+        + (alpha.checked ? alphaSaturation : `f`)
+    } )
+  } )
+} )
